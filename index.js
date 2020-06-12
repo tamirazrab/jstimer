71cdf7f4da2f87023132d2ceab36012589f4fb9a
tamirazrab <tamirazrab@gmail.com>
2020-06-12 15:04:36 +0500

    Finish with basic layout

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ class Timer {
 		this.pauseButton = pauseButton;
 
 		startButton.addEventListener("click", this.start);
+		pauseButton.addEventListener("click", this.pause);
 	}
 
 	start = () => {
-		console.log("Hellos there.");
-		console.log(`You told me to start timer for ${durationText}`);
-		// Just a reminder if this start() function is called through event listener
-		// this itself will point to the button element not to the instance of class.
 		/*
+			* Just a reminder if this start() function is called through event listener
+			 * this itself will point to the button element not to the instance of class.
+			 
 			- Arrow function will solve this problem as the value of this in arrow function
 			- will be equal to the value of this outside of arrow function.
 			- If this modern style js is converted into old js using babel.
@@ -20,6 +20,18 @@ class Timer {
 			- outside of that arrow function is constructor in which this points
 			- to the instance of class.
 		*/
+
+		this.intervalId = setInterval(this.tick, 1000); // calls the method every 1kms which is one second
+		// it also returns integer ID which represents this interval going on - which can
+		// later be used to cancel out this timer.
+	};
+
+	pause = () => {
+		clearInterval(this.intervalId);
+	};
+
+	tick = () => {
+		console.log("Coming up every second.");
 	};
 }
 
