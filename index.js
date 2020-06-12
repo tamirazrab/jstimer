177d365d0497b3aa198f54eb4d4d53bcd4ed45a0
tamirazrab <tamirazrab@gmail.com>
2020-06-12 14:49:50 +0500

    fixed this issue inside function call through event listener

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ class Timer {
 		startButton.addEventListener("click", this.start);
 	}
 
-	start() {
-		console.log("Hello there.");
-		console.log(`You tolrddrd me to start timer for ${durationText}`);
+	start = () => {
+		console.log("Hellos there.");
+		console.log(`You told me to start timer for ${durationText}`);
 		// Just a reminder if this start() function is called through event listener
 		// this itself will point to the button element not to the instance of class.
-	}
+		/*
+			- Arrow function will solve this problem as the value of this in arrow function
+			- will be equal to the value of this outside of arrow function.
+			- If this modern style js is converted into old js using babel.
+			- start function would be seen defined under constructor and since
+			- outside of that arrow function is constructor in which this points
+			- to the instance of class.
+		*/
+	};
 }
 
 let startButton = document.querySelector("#start-button");
