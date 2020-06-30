17f15fc327f0d17c812785cb49066b879e70e998
tamirazrab <tamirazrab@gmail.com>
2020-06-30 22:42:40 +0500

    Added blocky animation

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ let startButton = document.querySelector("#start-button");
 let pauseButton = document.querySelector("#pause-button");
 let durationText = document.querySelector(".duration-input");
 
-let myTimer = new Timer(durationText, startButton, pauseButton);
+let circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+
+circle.setAttribute("stroke-dasharray", perimeter);
+let currentOffset = 0;
+
+let myTimer = new Timer(durationText, startButton, pauseButton, {
+	onStart() {
+		console.log("Timer began");
+	},
+	onTick() {
+		circle.setAttribute("stroke-dashoffset", currentOffset);
+		currentOffset = currentOffset - 60;
+	},
+	onComplete() {
+		console.log("Timer completed");
+	},
+});
